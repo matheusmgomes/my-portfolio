bab131f481f276c90908addcac096980c80ab2f0
Matheus Machado <machado_gomes@hotmail.com>
2026-01-14 19:16:29 -0300

    creating delete and insert routes

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -10,3 +10,34 @@ exports.getProjects = async (req, res) => {
     res.status(500).json();
   }
 };
+
+exports.getProjectById = async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const [rows] = await conn.query(
+      `SELECT * from portfolio where id = ${id};`
+    );
+
+    res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json();
+  }
+};
+
+exports.createProject = async (req, res) => {
+  console.log(req.body);
+  res.status(200).json();
+};
+
+exports.deleteProject = async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    await conn.query(`DELETE FROM portfolio where id = ${id};`);
+
+    res.status(200).json({ message: "row deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json();
+  }
+};
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -4,5 +4,8 @@ const controller = require("../controllers/projectController");
 const router = express.Router();
 
 router.get("/", controller.getProjects);
+router.get("/:id", controller.getProjectById);
+router.post("/", controller.createProject);
+router.delete("/:id", controller.deleteProject);
 
 module.exports = router;
